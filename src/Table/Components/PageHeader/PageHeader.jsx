d9c8c728463c404711f810344c75e74cb368c254
Lisa Mabley <lisamabley@gmail.com>
2023-06-08 14:46:54 -0500

    feat: Initial commit

---
+++ src/Table/Components/PageHeader/PageHeader.jsx
@@ -0,0 +1,13 @@
+import './PageHeader.css';
+
+function PageHeader() {
+    return (
+        <header>
+            <div class="page-header">
+                <h1>Salary Calculator</h1>
+            </div>
+        </header>
+    );
+  }
+  
+  export default PageHeader;
